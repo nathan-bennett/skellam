6c75416fa2962969e07800b6f2dc464c7f75ea21
nathan <nathan@swipejobs.com>
2020-02-18 20:56:20 +1100

    give the ability to give two different arrays/matrices/series/dataframes to the two distributions

--- metrics/skellam_metrics.py
+++ metrics/skellam_metrics.py
@@ -6,16 +6,17 @@ import pandas as pd
 
 class SkellamMetrics:
 
-    def __init__(self, x, y, y_hat, model, l1, l2):
-        self._x = x
+    def __init__(self, x, y, y_hat, model, l0, l1):
         self._y = y
         self._y_hat = y_hat
         self.model = model
+        self.l0 = self.convert_to_array(l0)
         self.l1 = self.convert_to_array(l1)
-        self.l2 = self.convert_to_array(l2)
-        self.coeff_size = len(model.x) // 2
-        self.lambda_1_coefficients = self.model.x[0: self.coeff_size].reshape(-1, 1)
-        self.lambda_2_coefficients = self.model.x[self.coeff_size:].reshape(-1, 1)
+        self._x0, self._x1 = self._split_or_duplicate_x(x)
+
+        self.coeff_size = self._x0.shape[1]
+        self.lambda_0_coefficients = self.model.x[0: self.coeff_size].reshape(-1, 1)
+        self.lambda_1_coefficients = self.model.x[self.coeff_size:].reshape(-1, 1)
 
     @staticmethod
     def convert_to_array(_x):
@@ -25,6 +26,27 @@ class SkellamMetrics:
             return _x.values.reshape(-1, 1)
         else:
             return _x
+        
+    def _split_or_duplicate_x(self, x):
+        """This function aims to to create x0 and x1 by either duplicating x, if x is an array or series, otherwise
+        if x is a list then we will split the list where the first element will be equal to x0 whilst the second
+        element will be equal to x1
+        """
+        x0 = None
+        x1 = None
+        if isinstance(x, np.ndarray) or isinstance(x, pd.core.series.Series) or isinstance(x, pd.core.frame.DataFrame):
+            x0, x1 = x, x
+        elif len(x) == 2:
+            if isinstance(x[0], np.ndarray) or isinstance(x[0], pd.core.series.Series) or isinstance(x, pd.core.frame.DataFrame):
+                x0 = self.convert_to_array(x[0])
+            if isinstance(x[1], np.ndarray) or isinstance(x[1], pd.core.series.Series) or isinstance(x, pd.core.frame.DataFrame):
+                x1 = self.convert_to_array(x[1])
+            else:
+                x0 = x[0]
+                x1 = x[1]
+        else:
+            raise ValueError("x must either be an a list of two arrays or a single array")
+        return x0, x1
 
     def sse(self):
         return ((self._y - self._y_hat)**2).sum()
@@ -44,8 +66,8 @@ class SkellamMetrics:
         """Create arrays for our predictions of the two Poisson distributions
         """
         _lambda = dict()
-        _lambda['1'] = self.convert_to_array(np.exp(np.squeeze(self._x @ self.lambda_1_coefficients)))
-        _lambda['2'] = self.convert_to_array(np.exp(np.squeeze(self._x @ self.lambda_2_coefficients)))
+        _lambda['0'] = self.convert_to_array(np.exp(np.squeeze(self._x0 @ self.lambda_0_coefficients)))
+        _lambda['1'] = self.convert_to_array(np.exp(np.squeeze(self._x1 @ self.lambda_1_coefficients)))
         return _lambda
 
     def _calculate_v(self):
@@ -53,8 +75,8 @@ class SkellamMetrics:
         """
         _lambda = self._calculate_lambda()
         _v = dict()
+        _v['0'] = np.diagflat(_lambda['0'])
         _v['1'] = np.diagflat(_lambda['1'])
-        _v['2'] = np.diagflat(_lambda['2'])
         return _v
 
     def _calculate_w(self):
@@ -63,8 +85,8 @@ class SkellamMetrics:
         """
         _lambda = self._calculate_lambda()
         _w = dict()
+        _w['0'] = np.diagflat((self.l0 - _lambda['0'].reshape(-1, 1)) ** 2)
         _w['1'] = np.diagflat((self.l1 - _lambda['1'].reshape(-1, 1)) ** 2)
-        _w['2'] = np.diagflat((self.l2 - _lambda['2'].reshape(-1, 1)) ** 2)
         return _w
 
     def _calculate_robust_covariance(self):
@@ -73,21 +95,22 @@ class SkellamMetrics:
         _v = self._calculate_v()
         _w = self._calculate_w()
         _robust_cov = dict()
-        _robust_cov['1'] = np.linalg.inv(np.dot(np.dot(self._x.T, _v['1']), self._x)) \
-                           * np.dot(np.dot(self._x.T, _w['1']), self._x) \
-                           * np.linalg.inv(np.dot(np.dot(self._x.T, _v['1']), self._x))
-        _robust_cov['2'] = np.linalg.inv(np.dot(np.dot(self._x.T, _v['2']), self._x)) \
-                           * np.dot(np.dot(self._x.T, _w['2']), self._x) \
-                           * np.linalg.inv(np.dot(np.dot(self._x.T, _v['2']), self._x))
+        _robust_cov['0'] = np.linalg.inv(np.dot(np.dot(self._x0.T, _v['0']), self._x0)) \
+                           * np.dot(np.dot(self._x0.T, _w['0']), self._x0) \
+                           * np.linalg.inv(np.dot(np.dot(self._x0.T, _v['0']), self._x0))
+        _robust_cov['1'] = np.linalg.inv(np.dot(np.dot(self._x1.T, _v['1']), self._x1)) \
+                           * np.dot(np.dot(self._x1.T, _w['1']), self._x1) \
+                           * np.linalg.inv(np.dot(np.dot(self._x1.T, _v['1']), self._x1))
         return _robust_cov
 
     def _calculate_robust_standard_errors(self):
-        """Calculate robust standard errors for our two sets of coefficients
+        """Calculate robust standard errors for our two sets of coefficients by taking the square root of the diagonal
+        values in the variance covariance matrices
         """
         _robust_cov = self._calculate_robust_covariance()
         _std_error = dict()
+        _std_error['0'] = np.sqrt(np.diag(_robust_cov['0']))
         _std_error['1'] = np.sqrt(np.diag(_robust_cov['1']))
-        _std_error['2'] = np.sqrt(np.diag(_robust_cov['2']))
         return _std_error
 
     def _calculate_z_values(self):
@@ -95,8 +118,8 @@ class SkellamMetrics:
         """
         _std_error = self._calculate_robust_standard_errors()
         _z_values = dict()
+        _z_values['0'] = self.lambda_0_coefficients[:, 0] / _std_error['0']
         _z_values['1'] = self.lambda_1_coefficients[:, 0] / _std_error['1']
-        _z_values['2'] = self.lambda_2_coefficients[:, 0] / _std_error['2']
         return _z_values
 
     def _calculate_p_values(self):
@@ -104,6 +127,6 @@ class SkellamMetrics:
         """
         _z_values = self._calculate_z_values()
         _p_values = dict()
+        _p_values['0'] = scipy.stats.norm.sf(abs(_z_values['0'])) * 2
         _p_values['1'] = scipy.stats.norm.sf(abs(_z_values['1'])) * 2
-        _p_values['2'] = scipy.stats.norm.sf(abs(_z_values['2'])) * 2
         return _p_values
