6456afae7a1485350478d2fb5227c5f96e204ea6
nathan <nathan@swipejobs.com>
2020-02-25 17:23:12 +1100

    reorder requirements.txt

--- setup.py
+++ setup.py
@@ -3,6 +3,8 @@ from setuptools import setup, find_packages
 with open("README.md", "r") as readme_file:
     readme = readme_file.read()
 
+requirements = ["libblas-dev>=3", "liblapack-dev>=liblapack"]
+
 setup(
     name="notebookc",
     version="0.0.1",
@@ -13,8 +15,9 @@ setup(
     long_description_content_type="text/markdown",
     url="https://github.com/nathan-bennett/skellam",
     packages=find_packages(),
+    install_requires=requirements,
     classifiers=[
-        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
         "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
     ],
 )
